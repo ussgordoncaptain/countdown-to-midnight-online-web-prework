1ba474213425cb028b43943b3038dcf05fbbfe92
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-09 01:44:14 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(start)
+  while (start>0)
+  puts "#{start} SECOND(S)!"
+  start -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(start)
+  while (start>0)
+  puts "#{start} SECOND(S)!"
+  start -= 1
+  sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
